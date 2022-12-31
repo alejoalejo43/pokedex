f247ae239923649fad97437afa4908f6f0fe0394
Oscar Alejandro Castillo Verano <55119670+alejoalejo43@users.noreply.github.com>
2022-12-31 06:27:25 -0500

    protecting home 1:00

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import RoutePeotected from './components/RoutePeotected';
+import HomeProtected from './components/HomeProtected';
+import RouteProtected from './components/RouteProtected';
 import Home from './pages/Home';
 import Pokedex from './pages/Pokedex';
 import Pokemon from './pages/Pokemon';
@@ -9,9 +10,11 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<HomeProtected />}>
+          <Route path="/" element={<Home />} />
+        </Route>
 
-        <Route element={<RoutePeotected />}>
+        <Route element={<RouteProtected />}>
           <Route path="/pokedex" element={<Pokedex />} />
           <Route path="/pokedex/:id" element={<Pokemon />} />
         </Route>
